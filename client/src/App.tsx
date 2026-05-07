299328cb7bd523565b92873cee026d8f33cfe0e0
rickmateus8-code <rickmateus8@gmail.com>
2026-05-06 22:50:21 -0300

    FIX: Importação de EngineBuilder no App.tsx para resolver ReferenceError.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -81,6 +81,7 @@ import HistoricoSP from "./pages/HistoricoSP";
 import HistoricoUNINTER from "./pages/HistoricoUNINTER";
 import EngineDashboard from "./pages/EngineDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
+import EngineBuilder from "./pages/EngineBuilder";
 import TemplateManager from "./pages/TemplateManager";
 import ReceitaCria from "./pages/ReceitaCria";
 import AtestadoEditar from "./pages/AtestadoEditar";
